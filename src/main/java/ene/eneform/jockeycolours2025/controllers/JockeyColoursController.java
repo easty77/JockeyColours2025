d89d362094eed98b63fdd6200a26ac2d134ef61a
easty77 <simon.east@eneconsulting.be>
2025-01-02 19:25:58 +0100

    AdditionalRaces query

--- src/main/java/ene/eneform/jockeycolours2025/controllers/JockeyColoursController.java
+++ src/main/java/ene/eneform/jockeycolours2025/controllers/JockeyColoursController.java
@@ -1,21 +1,31 @@
 package ene.eneform.jockeycolours2025.controllers;
 
+import ene.eneform.jockeycolours2025.entities.AdditionalRaceMonthItem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 @Slf4j
 public class JockeyColoursController {
-
+@Autowired
+private final EntityManager entityManager;
         @GetMapping("/")
         public String index(ModelMap model) {
             //log.info("In RestController");
             System.out.println("In RestController");
-            model.put("data", "hello data");
+            TypedQuery<AdditionalRaceMonthItem> q = entityManager.createNamedQuery("selectAdditionalRaceMonth", AdditionalRaceMonthItem.class);
+            q.setParameter("monthOffset", 0);
+            List<AdditionalRaceMonthItem> items = q.getResultList();
+            model.put("races", items);
             return "index1";
         }
 }
